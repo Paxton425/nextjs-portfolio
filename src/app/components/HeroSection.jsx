ceb78b4c07feb621ecfde26f8d923227cc1c2614
Paxton425 <phillgaxsphe@gmail.com>
2025-11-03 07:02:40 +0200

    Intro Updates & Animation adjustments

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -8,14 +8,14 @@ import Link from "next/link";
 const HeroSection = () => {
   return (
     <section className="lg:py-16">
-      <div className="grid grid-cols-1 sm:grid-cols-12">
+      <div className="grid grid-cols-1 bg-white sm:grid-cols-12">
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start h-[600px]"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal lg:h-[250px] font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-600 to-pink-500">
               Hello, I&apos;m{" "}
             </span>
@@ -27,7 +27,7 @@ const HeroSection = () => {
                 "A Web Developer",
                 1000,
 				        "A React & Next.js Developer",
-				        2000,
+				        1000,
                 "A .NET Developer",
                 1000,
               ]}
@@ -36,13 +36,15 @@ const HeroSection = () => {
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Hello My Name is Sphelele, Wellcome To my Portfolio.
+          <p className="text-[#ADB7BE] text-base sm:text-lg lg:mr-2 mb-6 lg:text-xl">
+            <strong className="mb-4 text-center">Hello My Name is Sphelele, Wellcome To my Portfolio</strong> <br />
+              Acollection of my accomplishments, skills, and expertise in the field of ICT - Software development. 
+              Browse through my portfolio to discover my projects, qualifications and passion for software development.
           </p>
           <div>
             <Link
               href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-600 to-pink-500 hover:bg-slate-200 text-white"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-600 to-pink-500 hover:bg-slate-500 text-white"
             >
               Hire Me
             </Link>
@@ -59,16 +61,16 @@ const HeroSection = () => {
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="col-span-4 place-self-center mt-4 lg:mt-0"
+          transition={{ duratsion: 0.5 }}
+          className="col-span-4 mt-0 mb-4 lg:mt-0"
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/hero-guy-image.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={400}
-              height={400}
+              width={350}
+              height={350}
             />
           </div>
         </motion.div>
